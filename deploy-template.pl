0be0050438d73ef8c38b5072439f92c11f0ad75d
Dio <splash@gmail.com>
2015-07-15 18:50:40 +0200

    rebuild

--- deploy-template.pl
+++ deploy-template.pl
@@ -11,6 +11,11 @@ use warnings;
 use POSIX qw(ceil floor);
 use VMware::VIRuntime;
 use VMware::VILib;
+use VMware::VICommon;
+
+use AppUtil::VMUtil;
+use AppUtil::HostUtil;
+use AppUtil::XMLInputUtil;
 
 # Ignore SSL warnings or invalid server warning
 $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
@@ -18,7 +23,7 @@ $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
 # Default VM config
 my $cpus = 1;
 my $memory = 1024;
-my $sourcevm = 'templatename';
+my $vmtemplate = 'templatename';
 my $vmhost = 'esxi-host';
 my $user = 'user';
 my $pass = 'pass';
@@ -32,13 +37,13 @@ my %opts = (
         vmhost => {
                 type => "=s",
                 help => "ESXi Host in cluster to deploy VM to",
-                required => 0,
+                required => 1,
         },
-        sourcevm => {
+        vmtemplate => {
                 type => "=s",
                 help => "Name of VM Template (source VM)",
-                required => 0,
-                default => $sourcevm,
+                required => 1,
+                default => $vmtemplate,
         },
         vmname => {
                 type => "=s",
@@ -67,6 +72,18 @@ my %opts = (
                 required => 0,
                 default => $cpus,
         },
+        filename => {
+                type => "=s",
+                help => "The name of the configuration specification file",
+                required => 0,
+                default => "vmclone.xml",
+        },
+        schema => {
+                type => "=s",
+                help => "The name of the schema file",
+                required => 0,
+                default => "vmclone.xsd",
+        },
 );
 
 Opts::add_options(%opts);
@@ -76,21 +93,19 @@ Opts::validate();
 my $vmname = Opts::get_option('vmname');
 
 sub deploy_template() {
-        my ($sourcevm, $datastore, $resourcepool, $cpus, $folder, $comp_res_view, $vm_views);
+        my ($vmtemplate, $datastore, $resourcepool, $cpus, $folder, $comp_res_view, $vm_views);
 
-        if (Opts::get_option('server')) { $vcenter = Opts::get_option('vcenter'); }
+        if (Opts::get_option('vcenter')) { $vcenter = Opts::get_option('vcenter'); }
         if (Opts::get_option('pass')) { $pass = Opts::get_option('pass'); }
         if (Opts::get_option('user')) { $user = Opts::get_option('user'); }
         if (Opts::get_option('datastore')) { $datastore = Opts::get_option('datastore'); }
-        if (Opts::get_option('sourcevm')) { $sourcevm = Opts::get_option('sourcevm'); }
+        if (Opts::get_option('vmtemplate')) { $vmtemplate = Opts::get_option('vmtemplate'); }
         if (Opts::get_option('vmhost')) { $vmhost = Opts::get_option('vmhost'); }
         if (Opts::get_option('cpus')) { $cpus = Opts::get_option('cpus'); }
 
-        my $username = Opts::set_option ('username',$user);
-        my $password = Opts::set_option ('password',$pass);
         my $url = Opts::set_option ('url',"https://$vcenter/sdk/webService");
 
-        $vm_views = Vim::find_entity_views( view_type => 'VirtualMachine', filter => { 'name' => $sourcevm } );
+        $vm_views = Vim::find_entity_views( view_type => 'VirtualMachine', filter => { 'name' => $vmtemplate } );
 
         if (@$vm_views) {
                 foreach (@$vm_views) {
@@ -115,9 +130,11 @@ sub deploy_template() {
 
                         my $relocate_spec = get_relocate_spec(%relocate_params);
                         my $config_spec = get_config_spec();
-                        my $clone_spec = VirtualMachineCloneSpec->new( powerOn => 1, template => 0, location => $relocate_spec, config => $config_spec );
+                        my $customization_spec = VMUtils::get_customization_spec(Opts::get_option('filename'));;
 
-                        Util::trace (0, "Deploying virtual machine from template " . $sourcevm . "...\n");
+                        my $clone_spec = VirtualMachineCloneSpec->new( powerOn => 1, template => 0, location => $relocate_spec, config => $config_spec, customization => $customization_spec );
+
+                        Util::trace (0, "Deploying virtual machine from template " . $vmtemplate . "...\n");
 
                         if (Opts::get_option('folder')) {
                                 my $folder_name = Opts::get_option('folder');
@@ -127,13 +144,68 @@ sub deploy_template() {
                         }
 
                         eval {
-                                $_->CloneVM( folder => $folder, name => $vmname, spec => $clone_spec );
-                                Util::trace (0, $vmname . " (template " . $sourcevm . ") successfully deployed.\n");
+                                $_->CloneVM(  folder => $folder,
+                                              name => $vmname,
+                                              spec => $clone_spec );
+                                Util::trace (0, $vmname . " (template " . $vmtemplate . ") successfully deployed.\n");
                         };
 
-                }
+                        print "aaaa\n";
+                        print $@->detail;
+                        print "aaaa\n";
+
+                        if ($@) {
+                           if (ref($@) eq 'SoapFault') {
+                              if (ref($@->detail) eq 'FileFault') {
+                                 Util::trace(0, "\nFailed to access the virtual "
+                                                ." machine files\n");
+                              }
+                              elsif (ref($@->detail) eq 'InvalidState') {
+                                 Util::trace(0,"The operation is not allowed "
+                                               ."in the current state.\n");
+                              }
+                              elsif (ref($@->detail) eq 'NotSupported') {
+                                 Util::trace(0," Operation is not supported by the "
+                                               ."current agent \n");
+                              }
+                              elsif (ref($@->detail) eq 'VmConfigFault') {
+                                 Util::trace(0,
+                                 "Virtual machine is not compatible with the destination host.\n");
+                              }
+                              elsif (ref($@->detail) eq 'InvalidPowerState') {
+                                 Util::trace(0,
+                                 "The attempted operation cannot be performed "
+                                 ."in the current state.\n");
+                              }
+                              elsif (ref($@->detail) eq 'DuplicateName') {
+                                 Util::trace(0,
+                                 "The name '$vmname' already exists\n");
+                              }
+                              elsif (ref($@->detail) eq 'NoDisksToCustomize') {
+                                 Util::trace(0, "\nThe virtual machine has no virtual disks that"
+                                              . " are suitable for customization or no guest"
+                                              . " is present on given virtual machine" . "\n");
+                              }
+                              elsif (ref($@->detail) eq 'HostNotConnected') {
+                                 Util::trace(0, "\nUnable to communicate with the remote host, "
+                                                ."since it is disconnected" . "\n");
+                              }
+                              elsif (ref($@->detail) eq 'UncustomizableGuest') {
+                                 Util::trace(0, "\nCustomization is not supported "
+                                                ."for the guest operating system" . "\n");
+                              }
+                              else {
+                                 Util::trace (0, "Fault" . $@ . ""   );
+                              }
+                           }
+                           else {
+                              print ref($@);
+#                              Util::trace (0, "Fault" . $@ . ""   );
+                           }
+                        }
+                      }
         } else {
-                Util::trace (0, "Virtual machine template not found: " . $sourcevm . "\n");
+                Util::trace (0, "Virtual machine template not found: " . $vmtemplate . "\n");
         }
 }
 
@@ -144,7 +216,6 @@ sub get_config_spec() {
         my $config_spec = VirtualMachineConfigSpec->new( name => $vmname, memoryMB => $memory, numCPUs => $cpus );
         return $config_spec;
 }
-
 
 sub get_relocate_spec() {
         my %args = @_;
@@ -202,7 +273,6 @@ sub get_datastore {
 
         return ( name => $name, mor => $mor );
 }
-
 
 Util::connect();
 
